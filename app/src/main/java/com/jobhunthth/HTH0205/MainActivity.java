f43ef72f532472e02442c41d23779c44633fea5f
tungdv1503 <103915641+tungdv1503@users.noreply.github.com>
2023-05-02 00:59:02 +0700

    tung
    1

---
+++ app/src/main/java/com/jobhunthth/HTH0205/MainActivity.java
@@ -0,0 +1,14 @@
+package com.jobhunthth.HTH0205;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
