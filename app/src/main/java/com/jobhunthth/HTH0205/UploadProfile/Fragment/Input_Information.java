dd826fe7e9432337f79c53b1519952d042c6a047
Gradi0us <hieunguyen20032302@gmail.com>
2023-05-05 17:26:34 +0700

    done Update_infor

--- app/src/main/java/com/jobhunthth/HTH0205/UploadProfile/Fragment/Input_Information.java
+++ app/src/main/java/com/jobhunthth/HTH0205/UploadProfile/Fragment/Input_Information.java
@@ -1,13 +1,9 @@
 package com.jobhunthth.HTH0205.UploadProfile.Fragment;
 
-import static android.app.appsearch.AppSearchResult.RESULT_OK;
-
 import android.content.Intent;
-import android.database.sqlite.SQLiteCursor;
-import android.database.sqlite.SQLiteCursorDriver;
-import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 
@@ -24,20 +20,24 @@ import android.widget.ImageView;
 import android.widget.ScrollView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.jobhunthth.HTH0205.R;
 import com.jobhunthth.HTH0205.UploadProfile.UploadProfile;
 
 import java.io.ByteArrayOutputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 
@@ -50,18 +50,31 @@ public class Input_Information extends Fragment {
     int RESULT_OK2 = 12344;
     ImageView visible;
     ScrollView invisible;
-    TextInputEditText phone_edt, name_edt, age_edt, university_edt, majors_edt, years_edt, address_edt, github_edt;
+    String currentUid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+    TextInputEditText phone_edt, name_edt, age_edt, university_edt, majors_edt, years_edt, address_edt, github_edt,educate1;
     Button cv, avatar, background, save;
     Bitmap bitmapcv,bitmapavt,bitmapbackgr;
+    FirebaseFirestore db;
+    FirebaseStorage storage;
+    StorageReference storageRef;
+
+  DocumentReference profileRef;
+
+
    // Bitmap scaledBitmap = Bitmap.createScaledBitmap(bitmapcv,bitmapavt,bitmapbackgr,50,50, true);
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.information_update, container, false);
+        db = FirebaseFirestore.getInstance();
+        storage = FirebaseStorage.getInstance();
+        storageRef = storage.getReference();
         visible = v.findViewById(R.id.visibleinfor);
+        profileRef = db.collection("profile").document(currentUid);
         invisible = v.findViewById(R.id.invisible);
         phone_edt = v.findViewById(R.id.phone_edt);
         name_edt = v.findViewById(R.id.name_edt);
+        educate1 = v.findViewById(R.id.educate);
         age_edt = v.findViewById(R.id.age_edt);
         university_edt = v.findViewById(R.id.university_edt);
         majors_edt = v.findViewById(R.id.majors_edt);
@@ -72,7 +85,33 @@ public class Input_Information extends Fragment {
         avatar = v.findViewById(R.id.avt);
         background = v.findViewById(R.id.backgr);
         save = v.findViewById(R.id.save);
+        profileRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                if (documentSnapshot.exists()) {
+                    String name = documentSnapshot.getString("name");
+                    String phone = documentSnapshot.getString("phone");
+                    String age = documentSnapshot.getString("age");//
+                    String educate = documentSnapshot.getString("educate");//
+                    String uni = documentSnapshot.getString("university");
+                    String majors = documentSnapshot.getString("majors");
+                    String yearsofexp = documentSnapshot.getString("years_of_exp");
+                    String address = documentSnapshot.getString("address");
+                    String githublink = documentSnapshot.getString("github");
 
+                    name_edt.setText(name);
+                    phone_edt.setText(phone);
+                  age_edt.setText(age);
+                    educate1.setText(educate);
+                    university_edt.setText(uni);
+                    majors_edt.setText(majors);
+                   years_edt.setText(yearsofexp);
+                    address_edt.setText(address);
+                    github_edt.setText(githublink);
+
+                }
+            }
+        });
         visible.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -105,58 +144,203 @@ public class Input_Information extends Fragment {
                 startActivityForResult(intent, REQUESTS_CODE_FOLDER2);
             }
         });
+
+
+        Map<String, Object> profileData = new HashMap<>();
         save.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 // Tạo một đối tượng HashMap chứa thông tin muốn lưu
-                Map<String, Object> profileData = new HashMap<>();
+
                 profileData.put("name", name_edt.getText().toString());
                 profileData.put("address", address_edt.getText().toString());
                 profileData.put("phone", phone_edt.getText().toString());
+                profileData.put("age", age_edt.getText().toString());
+                profileData.put("educate", educate1.getText().toString());
                 profileData.put("university", university_edt.getText().toString());
                 profileData.put("majors", majors_edt.getText().toString());
                 profileData.put("years_of_exp", years_edt.getText().toString());
                 profileData.put("github", github_edt.getText().toString());
 
+                String currentUid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
+// Lấy dữ liệu từ Firestore với ID là currentUid
+                db.collection("profile").document(currentUid).get()
+                        .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                            @Override
+                            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                                if (documentSnapshot.exists()) {
+                                    // Nếu có tài liệu có ID trùng với currentUid, tiến hành update dữ liệu
+                                    db.collection("profile").document(currentUid).update(profileData)
+                                            .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                                @Override
+                                                public void onSuccess(Void aVoid) {
+                                                    Toast.makeText(getContext(), "Update successful", Toast.LENGTH_SHORT).show();
+                                                }
+                                            })
+                                            .addOnFailureListener(new OnFailureListener() {
+                                                @Override
+                                                public void onFailure(@NonNull Exception e) {
+                                                    Toast.makeText(getContext(), "Update failed", Toast.LENGTH_SHORT).show();
+                                                }
+                                            });
+                                } else {
+                                    // Nếu không có tài liệu nào có ID trùng với currentUid, tiến hành thêm mới
+                                    db.collection("profile").document(currentUid).set(profileData)
+                                            .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                                @Override
+                                                public void onSuccess(Void aVoid) {
+                                                    Toast.makeText(getContext(), "Save successful", Toast.LENGTH_SHORT).show();
+                                                }
+                                            })
+                                            .addOnFailureListener(new OnFailureListener() {
+                                                @Override
+                                                public void onFailure(@NonNull Exception e) {
+                                                    Toast.makeText(getContext(), "Save failed", Toast.LENGTH_SHORT).show();
+                                                }
+                                            });
+                                }
+                            }
+                        });
                 // Chuyển đổi bitmapcv thành một mảng byte và lưu vào Firestore dưới dạng base64 string
+
+                StorageReference cvRef = storageRef.child("images/" + currentUid + "/cv.jpg");
                 ByteArrayOutputStream cvStream = new ByteArrayOutputStream();
                 bitmapcv.compress(Bitmap.CompressFormat.JPEG, 100, cvStream);
                 byte[] cvBytes = cvStream.toByteArray();
-                String cvBase64 = Base64.encodeToString(cvBytes, Base64.DEFAULT);
-                profileData.put("bitmapcv", cvBase64);
+                UploadTask uploadTask = cvRef.putBytes(cvBytes);
+                uploadTask.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        cvRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                            @Override
+                            public void onSuccess(Uri uri) {
+                                profileData.put("cvUrl", uri.toString());
+                                db.collection("profile").document(currentUid).update("cvUrl", uri.toString());
+                            }
+                        });
+                    }
+                });
 
-                // Chuyển đổi bitmapavt thành một mảng byte và lưu vào Firestore dưới dạng base64 string
+// Lưu ảnh đại diện
+                StorageReference Sfavt = storageRef.child("images/" + currentUid + "/avt.jpg");
                 ByteArrayOutputStream avtStream = new ByteArrayOutputStream();
                 bitmapavt.compress(Bitmap.CompressFormat.JPEG, 100, avtStream);
                 byte[] avtBytes = avtStream.toByteArray();
-                String avtBase64 = Base64.encodeToString(avtBytes, Base64.DEFAULT);
-                profileData.put("bitmapavt", avtBase64);
+                UploadTask uploadTask1 = Sfavt.putBytes(avtBytes);
+                uploadTask1.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        Sfavt.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                            @Override
+                            public void onSuccess(Uri uri) {
+                                profileData.put("avtUrl", uri.toString());
+                                db.collection("profile").document(currentUid).update("avtUrl", uri.toString());
+                            }
+                        });
+                    }
+                });
 
-                // Chuyển đổi bitmapbackgr thành một mảng byte và lưu vào Firestore dưới dạng base64 string
+// Lưu ảnh nền
+                StorageReference backgrRef = storageRef.child("images/" + currentUid + "/backgr.jpg");
                 ByteArrayOutputStream backgrStream = new ByteArrayOutputStream();
                 bitmapbackgr.compress(Bitmap.CompressFormat.JPEG, 100, backgrStream);
                 byte[] backgrBytes = backgrStream.toByteArray();
-                String backgrBase64 = Base64.encodeToString(backgrBytes, Base64.DEFAULT);
-                profileData.put("bitmapbackgr", backgrBase64);
-
-                // Ghi dữ liệu vào Firestore với tên tài liệu là uid của tài khoản đang đăng nhập
-                FirebaseFirestore db = FirebaseFirestore.getInstance();
-                String currentUid = FirebaseAuth.getInstance().getCurrentUser().getUid();
-                db.collection("profile").document(currentUid).set(profileData)
-                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                UploadTask uploadTask2 = backgrRef.putBytes(backgrBytes);
+                uploadTask2.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        backgrRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
                             @Override
-                            public void onSuccess(Void aVoid) {
-                                Toast.makeText(getContext(), "oke", Toast.LENGTH_SHORT).show();
-                            }
-                        })
-                        .addOnFailureListener(new OnFailureListener() {
-                            @Override
-                            public void onFailure(@NonNull Exception e) {
-                                Toast.makeText(getContext(), "oke", Toast.LENGTH_SHORT).show();
+                            public void onSuccess(Uri uri) {
+                                profileData.put("backgrUrl", uri.toString());
+                                db.collection("profile").document(currentUid).update("backgrUrl", uri.toString());
                             }
                         });
+                    }
+                });
+
+//                // Chuyển đổi bitmapavt thành một mảng byte và lưu vào Firestore dưới dạng base64 string
+//                ByteArrayOutputStream avtStream = new ByteArrayOutputStream();
+//                bitmapavt.compress(Bitmap.CompressFormat.JPEG, 100, avtStream);
+//                byte[] avtBytes = avtStream.toByteArray();
+//                String avtBase64 = Base64.encodeToString(avtBytes, Base64.DEFAULT);
+//                profileData.put("bitmapavt", avtBase64);
+//
+//                // Chuyển đổi bitmapbackgr thành một mảng byte và lưu vào Firestore dưới dạng base64 string
+//                ByteArrayOutputStream backgrStream = new ByteArrayOutputStream();
+//                bitmapbackgr.compress(Bitmap.CompressFormat.JPEG, 100, backgrStream);
+//                byte[] backgrBytes = backgrStream.toByteArray();
+//                String backgrBase64 = Base64.encodeToString(backgrBytes, Base64.DEFAULT);
+//                profileData.put("bitmapbackgr", backgrBase64);
+//
+//                // Ghi dữ liệu vào Firestore với tên tài liệu là uid của tài khoản đang đăng nhập
+//                FirebaseFirestore db = FirebaseFirestore.getInstance();
+//                String currentUid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+//                db.collection("profile").document(currentUid).set(profileData)
+//                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+//                            @Override
+//                            public void onSuccess(Void aVoid) {
+//                                Toast.makeText(getContext(), "oke", Toast.LENGTH_SHORT).show();
+//                            }
+//                        })
+//                        .addOnFailureListener(new OnFailureListener() {
+//                            @Override
+//                            public void onFailure(@NonNull Exception e) {
+//                                Toast.makeText(getContext(), "oke", Toast.LENGTH_SHORT).show();
+//                            }
+//                        });
             }
         });
+//        save.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                // Tạo một đối tượng HashMap chứa thông tin muốn lưu
+//                FirebaseFirestore db = FirebaseFirestore.getInstance();
+//                String currentUid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+//                DocumentReference profileRef = db.collection("profile_info").document(currentUid);
+//                Map<String, Object> profileData = new HashMap<>();
+//                profileData.put("name", name_edt.getText().toString());
+//                profileData.put("address", address_edt.getText().toString());
+//                profileData.put("phone", phone_edt.getText().toString());
+//                profileData.put("university", university_edt.getText().toString());
+//                profileData.put("majors", majors_edt.getText().toString());
+//                profileData.put("years_of_exp", years_edt.getText().toString());
+//                profileData.put("github", github_edt.getText().toString());
+//                profileRef.set(profileData);
+//
+//                // Lưu avatar vào bộ sưu tập avatar
+//                DocumentReference avatarRef = db.collection("avatar").document(currentUid);
+//                ByteArrayOutputStream avtStream = new ByteArrayOutputStream();
+//                bitmapavt.compress(Bitmap.CompressFormat.JPEG, 100, avtStream);
+//                byte[] avtBytes = avtStream.toByteArray();
+//                String avtBase64 = Base64.encodeToString(avtBytes, Base64.DEFAULT);
+//                Map<String, Object> avatarData = new HashMap<>();
+//                avatarData.put("bitmapavt", avtBase64);
+//                avatarRef.set(avatarData);
+//
+//                // Lưu background vào bộ sưu tập background
+//                DocumentReference backgroundRef = db.collection("background").document(currentUid);
+//                ByteArrayOutputStream backgrStream = new ByteArrayOutputStream();
+//                bitmapbackgr.compress(Bitmap.CompressFormat.JPEG, 100, backgrStream);
+//                byte[] backgrBytes = backgrStream.toByteArray();
+//                String backgrBase64 = Base64.encodeToString(backgrBytes, Base64.DEFAULT);
+//                Map<String, Object> backgroundData = new HashMap<>();
+//                backgroundData.put("bitmapbackgr", backgrBase64);
+//                backgroundRef.set(backgroundData);
+//
+//                // Lưu cv vào bộ sưu tập cv
+//                DocumentReference cvRef = db.collection("cv").document(currentUid);
+//                ByteArrayOutputStream cvStream = new ByteArrayOutputStream();
+//                bitmapcv.compress(Bitmap.CompressFormat.JPEG, 100, cvStream);
+//                byte[] cvBytes = cvStream.toByteArray();
+//                String cvBase64 = Base64.encodeToString(cvBytes, Base64.DEFAULT);
+//                Map<String, Object> cvData = new HashMap<>();
+//                cvData.put("bitmapcv", cvBase64);
+//                cvRef.set(cvData);
+//
+//            }});
+
 
         return v;
     }
