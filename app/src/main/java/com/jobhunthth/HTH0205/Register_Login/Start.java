47e257671a98573c36cb0d6889c05bbb868240a5
L. J Nguyen <loinguyengamer2504@gmail.com>
2023-05-15 04:04:52 +0700

    Update StartActivity

--- app/src/main/java/com/jobhunthth/HTH0205/Register_Login/Start.java
+++ app/src/main/java/com/jobhunthth/HTH0205/Register_Login/Start.java
@@ -1,19 +1,28 @@
 package com.jobhunthth.HTH0205.Register_Login;
-
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.os.Handler;
+import android.os.SystemClock;
 import android.view.View;
-import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.LinearInterpolator;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.jobhunthth.HTH0205.R;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Start extends AppCompatActivity {
+
+    ImageView logo;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,45 +30,59 @@ public class Start extends AppCompatActivity {
         setContentView(R.layout.activity_start);
 
         // Find the logo ImageView and loading ProgressBar
-        ImageView logoImageView = findViewById(R.id.logoImageView);
-        ProgressBar loadingProgressBar = findViewById(R.id.loadingProgressBar);
+        logo = findViewById(R.id.logoImageView);
+        progressBar = findViewById(R.id.loadingProgressBar);
 
         // Zoom in the logo for 3 seconds
-        ObjectAnimator animator = ObjectAnimator.ofFloat(logoImageView, "scaleX", 1.0f, 1.2f);
-        animator.setDuration(3000);
-        animator.setInterpolator(new AccelerateDecelerateInterpolator());
-        animator.start();
+        ObjectAnimator ani1 = ObjectAnimator.ofFloat(logo, "alpha", 0f, 1f);
+        ObjectAnimator aniX = ObjectAnimator.ofFloat(logo, "scaleX", 0f, 2.5f);
+        ObjectAnimator aniY = ObjectAnimator.ofFloat(logo, "scaleY", 0f, 2.5f);
+        ani1.setDuration(1500);
+        aniX.setDuration(1500);
+        aniY.setDuration(1500);
 
-        ObjectAnimator animator2 = ObjectAnimator.ofFloat(logoImageView, "scaleY", 1.0f, 1.2f);
-        animator2.setDuration(3000);
-        animator2.setInterpolator(new AccelerateDecelerateInterpolator());
-        animator2.start();
+        AnimatorSet set = new AnimatorSet();
+        set.play(ani1).with(aniX).with(aniY);
+        set.start();
+        aniprogress();
+    }
 
-        // Move up the logo a little bit after the zoom animation
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                ObjectAnimator animator = ObjectAnimator.ofFloat(logoImageView, "translationY", -50f);
-                animator.setDuration(500);
-                animator.start();
+        public void aniprogress() {
+//        if(android.os.Build.VERSION.SDK_INT >= 11){
+            // will update the "progress" propriety of seekbar until it reaches progress
+            doStartProgressBar();
+            ObjectAnimator animation = ObjectAnimator.ofInt(progressBar, "progress", 0, 30000);
+            animation.setDuration(3000); // 0.5 second
+            animation.setInterpolator(new LinearInterpolator());
+            animation.start();
+            CountDownTimer countDownTimer = new CountDownTimer(3000, 1000) {
+                @Override
+                public void onTick(long l) {
+
+                }
+
+                @Override
+                public void onFinish() {
+                    startActivity(new Intent(Start.this, Register.class));
+                    finish();
+                }
+            }.start();
+//        }
+        }
+
+    private void doStartProgressBar() {
+        final int MAX = 100;
+        this.progressBar.setMax(MAX);
+        int progressIncrement = (int) ((2000 / (float) MAX) + 0.5);
+
+        for (int i = 0; i < MAX; i++) {
+            final int progress = i + 1;
+            SystemClock.sleep(progressIncrement);
+
+            progressBar.setProgress(progress);
+            if (progress == MAX) {
+                Toast.makeText(Start.this, "Chào Mừng", Toast.LENGTH_SHORT).show();
             }
-        }, 3000);
-
-        // Simulate some loading process with a delay
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                // Hide the logo and show the loading animation
-                logoImageView.animate().alpha(0).setDuration(300);
-                loadingProgressBar.setVisibility(View.VISIBLE);
-
-                // Start your main activity here
-                Intent intent = new Intent(Start.this, Login.class);
-                startActivity(intent);
-
-                // Close this activity
-                finish();
-            }
-        },0);
+        }
     }
 }
